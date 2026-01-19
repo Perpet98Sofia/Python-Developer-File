79e21e774cc2c9589a3905fced0e070673824cb8
Perpetua <134222072+Perpet98Sofia@users.noreply.github.com>
2026-01-19 16:17:58 +0300

    Intro to Python; variables, loops and def functions

---
+++ My_room2_assignment.py
@@ -0,0 +1,20 @@
+#1.Create a variable called age and store your age. Print it.
+#age = 25
+#print(age)
+
+#2. Create two variables a and b with values 10 and 5. Print their sum
+#a = 10
+#b = 5
+#print(a+b)
+
+#3. Multiply two numbers and print the result.
+#a,b = 10,5
+#print(a * b)
+
+#4. Divide 20 by 4 and print the result.
+print(20 // 4)
+print(20 / 4)
+
+#5. Write a program that prints “Python is fun”
+#program = "Python is fun"
+#print(program)

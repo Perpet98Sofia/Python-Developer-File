5105ef97b5b0c5a11f2090106acb6a2a7950cdcc
Perpetua <134222072+Perpet98Sofia@users.noreply.github.com>
2026-02-02 12:20:14 +0300

    ML with Regression(linear & logistic)

---
+++ Machine_Learning_Introduction.py
@@ -0,0 +1,27 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
+
+data = { 
+    'Age': [22, 25, 47, 52, 46, 56, 21, 55, 60, 25],
+    'Salary': [20000, 43000, 150000, 120000, 90000, 130000, 18000, 160000, 200000, 45000],
+    'Bought': [0, 0, 1, 1, 1, 1, 0, 1, 1, 0]
+    }
+
+#Convert the dictionary into a pandas DataFrame
+df = pd.DataFrame(data)
+X = df[['Age', 'Salary']]  # Features
+y = df['Bought']        # Target variable
+
+# Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
+
+#train
+model = LogisticRegression()
+model.fit(x_train, y_train)
+print('This training is successful. The model has learned the pattern')
+
+#predict
+y_pred = model.predict(x_test)

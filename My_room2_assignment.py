b3ef232580d031d97804bedaf5664afd9c2cd828
Perpetua <134222072+Perpet98Sofia@users.noreply.github.com>
2026-02-10 12:02:56 +0300

    exercise 2 assignment on def function

--- My_room2_assignment.py
+++ My_room2_assignment.py
@@ -18,3 +18,91 @@ print(20 / 4)
 #5. Write a program that prints “Python is fun”
 #program = "Python is fun"
 #print(program)
+
+#Exercise 1
+# Create a class called Student.
+class Student:
+    pass
+
+# Exercise 2
+# Add an __init__ method that takes name and age.
+
+class Student:
+    def __init__(self, name, age):
+        pass
+
+# Exercise 3
+# Inside the __init__ method, store name and age using self. 
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Exercise 4
+# Create a Student object and print the name.
+student1 = Student("Diana", 15)
+print(student1.name)
+
+# Exercise 5
+# Create a method called introduction that prints the student's name and age.
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduction(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+student2 = Student("Vicky", 16)
+student2.introduction()
+
+# Exercise 6
+# Add a grade attribute to the Student class.
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+# Exercise 7
+# Create a Student object with grade and print the grade.
+
+student3 = Student("Diana", 15, 80)
+print(student3.grade)
+
+# Exercise 8
+# Update the student's grade and print the new grade.
+
+student3.grade = 87
+print(student3.grade)
+
+# Exercise 9
+# Add a method age_group that prints "Adult" if age >= 18 else "Minor".
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def age_group(self):
+        if self.age >= 18:
+            print("Adult")
+        else:
+            print("Minor")
+
+student4 = Student("Mary", 10, 67)
+student4.age_group()
+
+# Exercise 10
+# Create three Student objects, store them in a list and print their names.
+ 
+students = [
+    Student("Dan", 14, 90),
+    Student("Grace", 18, 78),
+    Student("Harry", 19, 69)
+]
+ 
+for student in students:
+    print(student.name)
---
+++ Room2_Exercise2.py
@@ -0,0 +1,52 @@
+#6. add a grade attribute to student class
+#7. create a student object with grade and print the grade
+#8. update the student's grade and print the new grade
+#9.add method called introduce that prints "Hello, my name is {name} and I am {age} years old."
+#10. call the introduce method for one student
+#11. add a method age_group that prints "Adult" if age >= 18 else "Minor"
+#12. create three student object and store them in a list. 
+#13. use loop to print the names of all students in the list
+#14. use loop to call the introduce method for all students in the list
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        if self.grade >= 90:
+            self.grade_letter = 'A'
+        elif self.grade >= 80:
+            self.grade_letter = 'B'
+        elif self.grade >= 70:
+            self.grade_letter = 'C'
+        elif self.grade >= 60:
+            self.grade_letter = 'D'
+        else:
+            self.grade_letter = 'F'
+    
+    def introduction(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+    def age_group(self):
+        if self.age >= 18:
+            print("adult")
+        else:
+            print("minor")
+
+students = [
+    Student("Conny", 23, 90),
+    Student("Perpetua", 21, 88),
+    Student("Eucabeth", 17, 60)
+    ]
+for student in students:
+    print(student.name)
+    student.age_group()
+
+for student in students:
+    student.introduction()
+
+#student1 = Student("Diane", 17, 85)
+#student2 = Student("Lovelyn", 20, 92)
+#print(student1.name)
+#student2.introduction()
+#print(student1.grade_letter)
+#print(student2.grade_letter)
